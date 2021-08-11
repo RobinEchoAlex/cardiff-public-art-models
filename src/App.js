86d48aeb59619ccbd0d500f838e16db1a80561a5
RobinEchoAlex <872576923@qq.com>
2021-08-11 12:33:41 +0100

    able to display gltf file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import fountain_img from './img/drinking_fountain.jpg';
 import './App.css';
+import ReactDOM from 'react-dom'
+import {Canvas} from '@react-three/fiber'
+import { Suspense } from 'react'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Environment, OrbitControls } from "@react-three/drei";
+
+
+function sceneFallBack(){
+  console.log("scene fall back")
+}
+
+function Scene() {
+  const gltf = useLoader(GLTFLoader, './skeleton/scene.gltf')
+  return (
+    <Suspense fallback={sceneFallBack}>
+      <OrbitControls />
+      <primitive object={gltf.scene} scale={0.5}/>
+      <Environment preset="sunset" background />
+    </Suspense>
+  )
+}
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./skeleton/scene.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.8} />
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Canvas">
+        <Canvas>
+          <Suspense fallback={null}>
+            <Model/>
+            <OrbitControls/>
+            <Environment preset="sunset" background/>
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   );
 }
 
 export default App;
+//<img src={fountain_img} className="model-image" height="500" width="400"/>
