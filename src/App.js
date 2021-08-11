6b4ede8d4edfc487208b19f6521fe3aed2d34526
RobinEchoAlex <872576923@qq.com>
2021-08-11 17:47:05 +0100

    add a List etc.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,90 @@
-import fountain_img from './img/drinking_fountain.jpg';
 import './App.css';
 import ReactDOM from 'react-dom'
 import {Canvas} from '@react-three/fiber'
-import { Suspense } from 'react'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { Environment, OrbitControls } from "@react-three/drei";
+import {Suspense} from 'react'
+import {useLoader} from '@react-three/fiber'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {Environment, OrbitControls} from "@react-three/drei";
+import {ModelList} from "./ModelList"
 
+var modelNames = ["skeleton", "skeleton2"]
+var modelInfos = []
+function readAllModels() {
+  // const fs = require('fs')
+  //
+  //
+  //   fs.readdirSync("./models").forEach(function (modelFolder){
+  //     fs.readFile("introduction.txt",'utf8',((err, data) => {
+  //         console.log(data);
+  //     }))
+  //   })
 
-function sceneFallBack(){
+  // for (let modelName in modelNames) {
+  //   const reader = new FileReader();
+  //   reader.onload = async (e)=>{
+  //     const text = e.target.result
+  //     console.log(text)
+  //     alert(text)
+  //   }
+  //   reader.readAsText(new File("./models/skeleton/introduction.txt"));
+  if (modelInfos.length!==0) return;
+
+  for (let i=0; i<modelNames.length;i++) {
+    let modelName = modelNames[i];
+    let path = './models/'+ modelName +'/introduction.txt'
+    console.log(path)
+    fetch(path)
+      .then((r) => r.text())
+      .then(text => {
+        console.log(text);
+      })
+  }
+
+}
+
+function sceneFallBack() {
   console.log("scene fall back")
 }
 
 function Scene() {
-  const gltf = useLoader(GLTFLoader, './skeleton/scene.gltf')
+  const gltf = useLoader(GLTFLoader, './models/skeleton/scene.gltf')
   return (
     <Suspense fallback={sceneFallBack}>
-      <OrbitControls />
+      <OrbitControls/>
       <primitive object={gltf.scene} scale={0.5}/>
-      <Environment preset="sunset" background />
+      <Environment preset="sunset" background/>
     </Suspense>
   )
 }
 
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./skeleton/scene.gltf");
+const Model = (props) => {
+  let path = './models/'+ props.name +'/scene.gltf'
+  const gltf = useLoader(GLTFLoader, path);
   return (
     <>
-      <primitive object={gltf.scene} scale={0.8} />
+      <primitive object={gltf.scene} scale={0.8}/>
     </>
   );
 };
 
+// const ModelList = (props) => {
+//   return (
+//     <div>
+//       <p> skeleton </p>
+//       <p> skeleton2 </p>
+//     </div>
+//   )
+// }
+
+
 function App() {
   return (
     <div className="App">
+      <ModelList/>
       <div className="Canvas">
         <Canvas>
           <Suspense fallback={null}>
-            <Model/>
+            <Model name="skeleton"/>
             <OrbitControls/>
             <Environment preset="sunset" background/>
           </Suspense>
---
+++ src/ModelList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export const ModelList = () => {
+
+  const classes = useStyles();
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem
+          button
+          selected={selectedIndex === 0}
+          onClick={(event) => handleListItemClick(event, 0)}
+        >
+          <ListItemText primary="Skeleton"/>
+        </ListItem>
+        <ListItem
+          button
+          selected={selectedIndex === 1}
+          onClick={(event) => handleListItemClick(event, 1)}
+        >
+          <ListItemText primary="Skeleton22"/>
+        </ListItem>
+      </List>
+    </div>
+  );
+}
+
