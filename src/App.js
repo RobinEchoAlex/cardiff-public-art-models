f6964130ef7cc6da6a31d9afec55e4bd7760ce41
RobinEchoAlex <872576923@qq.com>
2021-08-12 23:25:01 +0100

    add photoUrlText and make row selectable from UI perspective

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -16,8 +16,8 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(folder,name, fbxUrl, photosUrl, coord, sketchfabUrl) {
-  return { folder, name, fbxUrl, photosUrl, coord , sketchfabUrl};
+function createData(folder, name, fbxUrl, photosUrl, photoUrlText, coord, sketchfabUrl) {
+  return {folder, name, fbxUrl, photosUrl, photoUrlText, coord, sketchfabUrl};
 }
 
 const rows = [
@@ -25,38 +25,51 @@ const rows = [
     "Memorial to Diverse Ethnic and Commonwealth People In War",
     "https://drive.google.com/file/d/1LNMuZXiT8NdX2J66xm80QdM64ZkXIm9_/view?usp=sharing",
     "#",
+    '',
     [51.485999751019435, -3.1802259356889366],
     "https://sketchfab.com/models/ca05b951486840cda1a45e7a890128f9/embed"),
   createData('skeleton2',
     'Drinking Fountain',
     "https://drive.google.com/file/d/1v5fbbISKRjkV5ZrtvZSmvoDCnhAFP8Fr/view?usp=sharing",
-    "#",
+    "https://dubox.com/s/1MASqbt5wlH7pJmiUOToYzw",
+    '5vux',
     [51.48374634245954, -3.1782340219244305],
     "https://sketchfab.com/models/a24b2438e3674b3b8c334d136dc89126/embed"),
   createData('magistrates',
     'Magistrate Court',
     "https://drive.google.com/file/d/1-q6LwWB1VO4VjxMqtHlYMdinxTKZPByG/view?usp=sharing",
-    "#",
+    "https://dubox.com/s/1DvC2F4VCx8Zj3cGj8XaQZQ",
+    'xgrs',
     [51.48152690383384, -3.1663288597426518]),
   createData('girl',
     'Girl',
     '',
-    '',
+    'https://dubox.com/s/1CXHVuStqZRE8HWiU6ho2IA',
+    'd439',
     [51.485165905517405, -3.1768937541292517])
 ];
 
-class BasicTable extends React.Component{
+class BasicTable extends React.Component {
   //const classes = useStyles();
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.isSelected = this.isSelected.bind(this);
+    this.state = ({
+      selected: "Memorial to Diverse Ethnic and Commonwealth People In War"
+    })
   }
 
-  handleClick (event, id){
-    this.props.onSelectedModelChanged(id);
+  handleClick(event, name) {
+    this.props.onSelectedModelChanged(name);
+    this.setState({selected: name})
   };
 
-  render(){
+  isSelected(name){
+    return name===this.state.selected;
+  }
+
+  render() {
     return (
       <TableContainer component={Paper}>
         <Table className={"table"} aria-label="simple table">
@@ -69,14 +82,21 @@ class BasicTable extends React.Component{
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name} hover={true} onClick={(e) => {this.handleClick(e,row.name)}}>
-                <TableCell ><img src={'./models/'+row.folder+'/image.jpg'} width="75" height="100"/></TableCell>
-                <TableCell component="th" scope="row">{row.name}</TableCell>
-                <TableCell align="right"><a href={row.fbxUrl}>Download</a></TableCell>
-                <TableCell align="right"><a href={row.photosUrl}>Download</a></TableCell>
-              </TableRow>
-            ))}
+            {//https://stackoverflow.com/questions/52313755/material-ui-table-how-to-make-cells-selectable
+              rows.map((row) => {
+                  const isSelected = this.isSelected(row.name);
+                  return (
+                    <TableRow key={row.name} hover={true} selected={isSelected} onClick={(e) => {
+                      this.handleClick(e, row.name)
+                    }}>
+                      <TableCell><img src={'./models/' + row.folder + '/image.jpg'} width="75" height="100"/></TableCell>
+                      <TableCell component="th" scope="row">{row.name}</TableCell>
+                      <TableCell align="right"><a href={row.fbxUrl}>Download</a></TableCell>
+                      <TableCell align="right"><a href={row.photosUrl}>Download</a><br/>{row.photoUrlText}</TableCell>
+                    </TableRow>
+                  )
+                }
+              )}
           </TableBody>
         </Table>
       </TableContainer>
@@ -84,19 +104,19 @@ class BasicTable extends React.Component{
   }
 }
 
-class SketchfabEmbedding extends  React.Component{
+class SketchfabEmbedding extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    if (this.props.title==null || this.props.url==null) return(
+    if (this.props.title == null || this.props.url == null) return (
       <iframe title="Drinking Fountain" frameBorder="0" allowFullScreen mozallowfullscreen="true"
               webkitallowfullscreen="true" allow="fullscreen; autoplay; vr" xr-spatial-tracking="true"
               execution-while-out-of-viewport="true" execution-while-not-rendered="true" web-share="true"
               src="https://sketchfab.com/models/a24b2438e3674b3b8c334d136dc89126/embed"></iframe>
     )
-    else return(
+    else return (
       <iframe title={this.props.title} frameBorder="0" allowFullScreen mozallowfullscreen="true"
               webkitallowfullscreen="true" allow="fullscreen; autoplay; vr" xr-spatial-tracking="true"
               execution-while-out-of-viewport="true" execution-while-not-rendered="true" web-share="true"
@@ -117,8 +137,10 @@ class App extends React.Component {
     this.setState({selectedModel: selectedModel});
   }
 
-  getObj(){//TODO map rather than array
-    return rows.find(obj=> {return obj.name===this.state.selectedModel})
+  getObj() {//TODO map rather than array
+    return rows.find(obj => {
+      return obj.name === this.state.selectedModel
+    })
   }
 
   render() {
@@ -126,10 +148,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1 id="title">Cardiff Public Art Model Repository - Site Under Construction </h1>
-        <p id="intro">This site displays 3D models created during the Cardiff University summer project Computational Culture Heritage. These models are reconstructed from photos with Meshroom and Autodesk Recap. All the model meshes and original photos are available to download.
+        <p id="intro">This site displays 3D models created during the Cardiff University summer project Computational
+          Culture Heritage. These models are reconstructed from photos with Meshroom and Autodesk Recap. All the model
+          meshes and original photos are available to download.
         </p>
         <div id="table">
-          <BasicTable onSelectedModelChanged={this.handleSelectedModelChanged} />
+          <BasicTable onSelectedModelChanged={this.handleSelectedModelChanged}/>
         </div>
         <div id="map">
           <Leaflet id="map" coord={obj.coord}/>
